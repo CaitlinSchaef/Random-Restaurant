168f879b9519f74c8d9e0e13567963be57475dc2
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-26 09:31:05 -0400

    in the middle of tweaks

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,26 +4,38 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Logo from '../assets/Logo4.png'
-import Info from '../assets/Logo4Contact.png'
-import Image from 'react-bootstrap/Image';
-import Sunset from '../assets/sardiniasunset.jpeg'
+import Info from '../assets/SquareLogo.png'
+
+
 
 const Body = () => {
   return (
+
     <ThemeProvider
   breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs']}
   minBreakpoint="xs">
   <div>
-    <Container className="mt-3 mb-3 specialContainer">
-      <Row className="justify-content-md-center">
+    <Container className="mt-3 mb-3">
+      <Row className="justify-content-md-center menuBody">
         <Col className="justify-content-center text-center mb-3" md="auto">
         <img
-              alt="Contact Information"
-              src={Info}
-              width="auto"
-              height="auto"
-              className="d-inline-block align-top mt-2"
-            />
+             alt="Contact Information"
+             src={Info}
+             width="auto"
+             height="auto"
+             className="d-inline-block align-top mt-2"
+             ></img>
+            <br></br>
+            <br></br>
+              <h3><strong>Contact</strong></h3>
+                <p>
+                348 E Main St, Lexington, KY 40507 <br></br>
+                (555)555-5555 <br></br>
+                Open: <br></br>
+                Wednesday-Sunday <br></br>
+                11:30am-2pm & 6pm-11pm
+                </p>
+              
         </Col>
       </Row>
     </Container>
@@ -31,6 +43,8 @@ const Body = () => {
 </ThemeProvider>
   )
 }
+
+
 
 function Contact() {
   return (
